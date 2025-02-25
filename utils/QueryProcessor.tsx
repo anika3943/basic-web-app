101d6456e24625dd0fe99bee104b20dcc7ab09db
anika3943 <anikas@andrew.cmu.edu>
2025-02-25 10:22:25 -0500

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -9,37 +9,48 @@ export default function QueryProcessor(query: string): string {
 
   query = query.toLowerCase();
 
-  if (query.toLowerCase().includes("andrew")) {
-    return (
-      "anikas"
-    );
+  if (query.includes("andrew")) {
+    return "anikas";
   }
-  
 
-  if (query.toLowerCase().includes("name")) {
+  if (query.includes("name")) {
     return "Anika Sharma";
   }
 
   if (query.includes("plus")) {
     let numbers = query.match(/\d+/g);
     if (numbers && numbers.length === 2) {
-        return `${parseInt(numbers[0], 10) + parseInt(numbers[1], 10)}`;
+      return `${parseInt(numbers[0], 10) + parseInt(numbers[1], 10)}`;
     }
-}
+  }
 
-if (query.includes("largest")) {
+  if (query.includes("largest")) {
     let numbers = query.match(/\d+/g);
     if (numbers) {
-        return `${Math.max(...numbers.map(num => parseInt(num, 10)))}`;
+      return `${Math.max(...numbers.map(num => parseInt(num, 10)))}`;
     }
-}
-
-if (query.includes("multiplied")) {
-  let numbers = query.match(/\d+/g);
-  if (numbers) {
-      return `${parseInt(numbers[0], 10) * parseInt(numbers[1], 10)}`;
   }
-}
+
+  if (query.includes("multiplied")) {
+    let numbers = query.match(/\d+/g);
+    if (numbers) {
+      return `${parseInt(numbers[0], 10) * parseInt(numbers[1], 10)}`;
+    }
+  }
+
+  if (query.includes("square") && query.includes("cube")) {
+    let numbers = query.match(/\d+/g);
+    if (numbers) {
+      let findSixthPower = numbers
+        .map(num => parseInt(num, 10))
+        .filter(num => {
+          let root = Math.round(Math.pow(num, 1 / 6));
+          return Math.pow(root, 6) === num;
+        });
+
+      return findSixthPower.length > 0 ? findSixthPower.join(", ") : "None";
+    }
+  }
 
   return "";
 }
